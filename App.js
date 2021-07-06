ccaf779f05622b30b8f78cf482d18285f8478c05
Dagorik <dagorik@gmail.com>
2021-07-06 17:00:58 -0500

    feat: new tyc

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// Imposta la libreria
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
+
+  // Se va a ejecutar una vez, cuando inicie la App
+  // La funcionalidad es de React
+  useEffect(() => {
+    // Solicitar los permisos para usar la camara
+    (async () => {
+      const {status} = await BarCodeScanner.requestPermissionsAsync();
+    })()
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola desde React Native</Text>
+      <Text>Welcome Talent</Text>
+
+      <BarCodeScanner
+        onBarCodeScanned={({ type, data }) => {
+          // Que voy a hacer con la info del QR
+          alert(data);
+        }}
+        style={StyleSheet.absoluteFillObject}
+       />
+ 
       <StatusBar style="auto" />
     </View>
   );
